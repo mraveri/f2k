31c9c52b97fa128ebf3b6b2b72d52e927ef974d0
Marco Raveri <mraveri@sissa.it>
2016-12-30 16:12:53 +0100

    Clean-up

--- html/search/functions_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['elapsed_5ftime',['elapsed_time',['../structf2k__timer_1_1timer.html#aa5aee6ebc58df5de0530d116b321f7fc',1,'f2k_timer::timer']]]
-];
--- html/search/variables_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['end_5ftime',['end_time',['../structf2k__timer_1_1timer.html#af5fd0bdd12fb2d3a6ceb05bc360382de',1,'f2k_timer::timer']]]
-];
